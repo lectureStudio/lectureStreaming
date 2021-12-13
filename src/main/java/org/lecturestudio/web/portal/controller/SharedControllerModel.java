46c01915a4dda8a5177ce28aed48167897cadb29
Alex Andres <andres.alex@protonmail.com>
2021-12-13 15:21:12 +0000

    Show app version in the site-footer

--- src/main/java/org/lecturestudio/web/portal/controller/SharedControllerModel.java
+++ src/main/java/org/lecturestudio/web/portal/controller/SharedControllerModel.java
@@ -4,6 +4,8 @@ import static java.util.Objects.nonNull;
 
 import org.lecturestudio.web.portal.saml.LectUserDetails;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.info.GitProperties;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -11,14 +13,22 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 @ControllerAdvice
 public class SharedControllerModel {
 
+	@Autowired
+	private GitProperties gitProperties;
+
+
 	@ModelAttribute("appName")
 	String appName() {
 		return "lectureStreaming";
+	}
+
+	@ModelAttribute("appVersion")
+	String appVersion() {
+		return gitProperties.get("build.version") + "-" + gitProperties.get("total.commit.count");
 	}
 
 	@ModelAttribute("user")
 	LectUserDetails userDetails(Authentication authentication) {
 		return nonNull(authentication) ? (LectUserDetails) authentication.getDetails() : null;
 	}
-
 }
